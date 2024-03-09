aa4e7fa2b110c1b65c424e1d0cb7edb946bc028b
Soockee <gamingsockee@gmail.com>
2024-03-09 16:45:10 +0100

    fix: add simple snake speed

--- ldtk-snake/systems/snake.go
+++ ldtk-snake/systems/snake.go
@@ -3,6 +3,7 @@ package systems
 import (
 	"image"
 	"log/slog"
+	"math"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	input "github.com/quasilyte/ebitengine-input"
@@ -18,9 +19,12 @@ const (
 	ActionClick
 )
 
+var snakeSpeed float64 = 3
+
 type Snake struct {
 	input     *input.Handler
 	snakehead *ldtkgo.Entity
+	speed     float64
 	direction input.Action // todo: not used yet
 	tile      *ebiten.Image
 }
@@ -45,22 +49,24 @@ func NewSnake(head *ldtkgo.Entity, renderer *helper.EbitenRenderer) *Snake {
 	snakehead := &Snake{
 		input:     InputSytem.NewHandler(0, snakeheadKeymap),
 		snakehead: head,
+		speed:     snakeSpeed,
 		tile:      tile,
 	}
 
 	return snakehead
 }
 
+// move temporarily uses a speed of type int whiel figuring out the collision
 func (s *Snake) move(action input.Action) {
 	switch action {
 	case ActionMoveLeft:
-		s.snakehead.Position[0]--
+		s.snakehead.Position[0] -= int(math.Ceil(s.speed))
 	case ActionMoveRight:
-		s.snakehead.Position[0]++
+		s.snakehead.Position[0] += int(math.Ceil(s.speed))
 	case ActionMoveUp:
-		s.snakehead.Position[1]--
+		s.snakehead.Position[1] -= int(math.Ceil(s.speed))
 	case ActionMoveDown:
-		s.snakehead.Position[1]++
+		s.snakehead.Position[1] += int(math.Ceil(s.speed))
 	default:
 		slog.Warn("invalid move key")
 	}
