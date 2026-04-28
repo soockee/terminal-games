edb86e35f835cef2370d376a913742653b64f744
Simon Stockhause <simon.stockhause@lhsystems.com>
2026-04-28 10:17:29 +0200

    fix(match-3): update template code

--- match-3/component/component.go
+++ match-3/component/component.go
@@ -4,11 +4,10 @@ import (
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/audio"
 	"github.com/solarlune/resolv"
 	"github.com/yohamta/donburi"
 )
-
-// ---- Position & physics ----
 
 // ---- Shape (resolv collision shape, owned per-entity) ----
 
@@ -46,6 +45,36 @@ var Color = donburi.NewComponentType[ColorData]()
 
 var Space = donburi.NewComponentType[resolv.Space]()
 
+// ---- Score (singleton) ----
+
+type ScoreData struct {
+	Value  int
+	Target int // number of objectives to win (0 = no win condition)
+}
+
+var Score = donburi.NewComponentType[ScoreData]()
+
+// ---- GameOver (singleton) ----
+
+type GameOverData struct {
+	Dead    bool
+	Started bool // false until first input
+	Restart bool // set by input to signal a level reload
+	Won     bool // true when win condition is met
+}
+
+var GameOver = donburi.NewComponentType[GameOverData]()
+
+// ---- Camera (singleton) ----
+
+type CameraData struct {
+	X      float64 // world-space X origin of the viewport
+	ScaleX float64 // virtual screen / world scale (horizontal)
+	ScaleY float64 // virtual screen / world scale (vertical)
+}
+
+var Camera = donburi.NewComponentType[CameraData]()
+
 // ---- Debug (singleton toggle) ----
 
 type DebugData struct {
@@ -53,3 +82,13 @@ type DebugData struct {
 }
 
 var Debug = donburi.NewComponentType[DebugData]()
+
+// ---- Audio (singleton) ----
+
+type AudioData struct {
+	Ctx     *audio.Context
+	BGMusic *audio.Player
+	SFX     map[string]*audio.Player
+}
+
+var Audio = donburi.NewComponentType[AudioData]()
--- match-3/tags/tags.go
+++ match-3/tags/tags.go
@@ -1,3 +1,17 @@
 package tags
 
-var ()
+import "github.com/yohamta/donburi"
+
+// Tag variables for entity queries.
+var (
+	Player     = donburi.NewTag().SetName("Player")
+	Collidable = donburi.NewTag().SetName("Collidable")
+	Ground     = donburi.NewTag().SetName("Ground")
+)
+
+// String constants for use in switch/label logic.
+const (
+	TagPlayer     = "Player"
+	TagCollidable = "Collidable"
+	TagGround     = "Ground"
+)
